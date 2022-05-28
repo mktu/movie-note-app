aef1e9c8b2a6342d4bd06decf73c1937e29dec11
mktu <mushi9ui@gmail.com>
2022-05-28 11:13:29 +0900

    add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import styles from "./styles/app.css"
 import type { MetaFunction } from "@remix-run/cloudflare";
 import {
   Links,
@@ -7,6 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
