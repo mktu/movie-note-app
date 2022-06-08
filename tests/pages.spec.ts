d59268b7b4b932ccde5948ae3871756b4067b1e0
mktu <mushi9ui@gmail.com>
2022-06-08 10:07:23 +0900

    add github actions

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -2,5 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
     await page.goto('http://localhost:8788/');
-    expect(await page.textContent('h1')).toBe('Welcome to Remix');
+    page.waitForURL('http://localhost:8788/login')
+    expect(await page.textContent('button')).toBe('Login with Google');
   });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
     test: {
         /* for example, use global to avoid globals imports (describe, test, expect): */
         // globals: true,
+        exclude: [...configDefaults.exclude, 'tests/*'],
     },
 })
