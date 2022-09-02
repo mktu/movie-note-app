4e3812ab706d8bf39a0ce495679d4d5c64440147
mktu <mushi9ui@gmail.com>
2022-09-03 00:53:35 +0900

    changed sidebar design

--- app/components/remix-routes/sidebar-items/StaticLinks.tsx
+++ app/components/remix-routes/sidebar-items/StaticLinks.tsx
@@ -1,5 +1,6 @@
 import HomeIcon from '~/components/icons/Home';
 import SearchIcon from '~/components/icons/Search';
+import SquarePlus from '~/components/icons/SquarePlus';
 
 import { NavLink } from '@remix-run/react';
 
@@ -8,14 +9,15 @@ import type { FC } from 'react'
 const links = [
     { name: 'HOME', icon: <HomeIcon className='h-5 w-5 fill-text-main' />, link: '/app' },
     { name: 'SEARCH', icon: <SearchIcon className='h-5 w-5 fill-text-main' />, link: '/search' },
+    { name: 'NEW NOTE', icon: <SquarePlus className='h-5 w-5 fill-text-main' />, link: '/app/new-note' },
 ]
 
 const StaticLinks: FC = () => {
     return (
-        <ul className=''>
+        <ul>
             {links.map(l => (
-                <li key={l.name} className='p-2'>
-                    <NavLink to={l.link} className='flex items-center text-text-main'>
+                <li key={l.name} className=''>
+                    <NavLink to={l.link} className='flex items-center p-2 text-text-main hover:bg-surface-hover'>
                         {l.icon}
                         <span className="ml-2">{l.name}</span>
                     </NavLink>
