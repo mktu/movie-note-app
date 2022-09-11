e5eba640bd3a90919b1cb1381a82e026ba744218
mktu <mushi9ui@gmail.com>
2022-09-11 21:29:37 +0900

    add limit to Credits

--- app/components/buttons/Text.tsx
+++ app/components/buttons/Text.tsx
@@ -2,16 +2,28 @@ import clsx from "clsx"
 import { forwardRef } from "react"
 import Base from './Base'
 
-type Props = Parameters<typeof Base>[0] & {
-    paddings?: string
+type Theme = 'none' | 'text' | 'label'
+
+const colors: { [t in Theme]: string } = {
+    none: '',
+    text: 'text-text-main hover:text-text-dark focus:text-text-dark',
+    label: 'text-text-label hover:text-text-main focus:text-text-main'
 }
+
+type Props = Parameters<typeof Base>[0] & {
+    paddings?: string,
+    theme?: Theme
+}
+
+
 
 const Text = forwardRef<HTMLButtonElement, Props>(({
     paddings = 'py-2 px-4',
+    theme = 'none',
     className,
     ...props
 }, ref) => (
-    <Base className={clsx(className,
+    <Base className={clsx(className, colors[theme],
         paddings)} {...props} ref={ref} />
 ))
 
