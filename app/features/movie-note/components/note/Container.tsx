86ee653580b050a03e37faf5e656353721a9518c
mktu <mushi9ui@gmail.com>
2022-09-22 00:58:34 +0900

    not use Suspense until related bug fixed

--- app/features/movie-note/components/note/Container.tsx
+++ app/features/movie-note/components/note/Container.tsx
@@ -1,16 +1,21 @@
 import type { ComponentProps, FC } from 'react';
-import { lazy, Suspense } from 'react'
-
-const Editor = lazy(() => import('./Editor'));
+import Editor from './Editor';
+// import { lazy, Suspense } from 'react'
+// const Editor = lazy(() => import('./Editor'));
 
 type Props = ComponentProps<typeof Editor>
 
 const Container: FC<Props> = (props) => {
     return (
-        <Suspense fallback={<div />}>
-            <Editor {...props} />
-        </Suspense>
-    );
+        <Editor {...props} />
+    )
+    // cannot use renderToReadableStream 
+    // (related isue: https://github.com/remix-run/remix/issues/2299#issuecomment-1091149019)
+    // return (
+    //     <Suspense fallback={<div />}>
+    //         <Editor {...props} />
+    //     </Suspense>
+    // );
 };
 
 export default Container;
