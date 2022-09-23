7a88be0144b2e77dd8f16d27202670df82abd6f6
mktu <mushi9ui@gmail.com>
2022-09-23 10:47:06 +0900

    refactor

--- app/routes/app/new-note.tsx
+++ app/routes/app/new-note.tsx
@@ -1,42 +1,12 @@
-import { json, redirect } from "@remix-run/cloudflare";
-import type { ActionArgs } from "@remix-run/cloudflare";
-import authenticator from "~/features/auth/server/auth.server";
-import { getSupabaseAdmin } from '@utils/server/db/index.server'
 import type { FC } from "react";
-import { NewMovieNote } from "~/features/movie-note/";
-import { registerMovieNote } from "~/features/movie-note/server/db";
-import { useActionData, useSubmit } from "@remix-run/react";
-import { MovieNoteError } from "~/features/movie-note/utils/error";
-import { getFormData } from "@utils/form";
-import { parseAddNote } from "~/features/movie-note/server/validation";
+import { NewMovieNote } from '~/features/movie-note/';
 
-type ActionData = {
-    error?: string
-}
+import { useActionData, useSubmit } from '@remix-run/react';
+import { getFormData } from '@utils/form';
 
-export async function action({ request, context }: ActionArgs) {
+import type { action } from "~/features/movie-note/server/actions/new-note.server";
 
-    const user = await authenticator.isAuthenticated(request)
-
-    if (!user) {
-        return redirect('/login')
-    }
-    const supabaseAdmin = getSupabaseAdmin(context)
-    try {
-        const data = parseAddNote(await request.formData())
-        await registerMovieNote(supabaseAdmin, data, user.id)
-        return redirect('/app')
-    } catch (e) {
-        if (e instanceof MovieNoteError) {
-            return json<ActionData>({
-                error: e.message
-            }, { status: e.status })
-        }
-        return json<ActionData>({
-            error: (e as Error).message
-        }, { status: 400 })
-    }
-};
+export { action }
 
 const NewNote: FC = () => {
     const submit = useSubmit()
