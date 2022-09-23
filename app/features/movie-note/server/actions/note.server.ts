2a6fbf2eea12e5ffab3095623c53869ab9886fca
mktu <mushi9ui@gmail.com>
2022-09-23 09:49:52 +0900

    kv values should be {[tmdbId] : [imdbId,lng]}

---
+++ app/features/movie-note/server/actions/note.server.ts
@@ -0,0 +1,42 @@
+import authenticator from '~/features/auth/server/auth.server';
+import { updateMovieNote } from '~/features/movie-note/server/db';
+import { parseUpdateNote } from '~/features/movie-note/server/validation/updateNote';
+import { MovieNoteError } from '~/features/movie-note/utils/error';
+
+import { json, redirect } from '@remix-run/cloudflare';
+import { getSupabaseAdmin } from '@utils/server/db/index.server';
+
+import type { ActionArgs } from "@remix-run/cloudflare";
+import { putMovieNoteIds } from '../kv/tmdb';
+
+type ActionData = {
+    error?: string
+}
+export async function action({ request, context }: ActionArgs) {
+
+    const user = await authenticator.isAuthenticated(request)
+
+    if (!user) {
+        return redirect('/login')
+    }
+    const supabaseAdmin = getSupabaseAdmin(context)
+    try {
+        const data = parseUpdateNote(await request.formData())
+        await updateMovieNote(supabaseAdmin, data, user.id)
+        await putMovieNoteIds(context.MovieNoteIds as KVNamespace, {
+            tmdbId: data.tmdbId,
+            imdbId: data.imdbId || null,
+            lng: data.lng
+        }, user.id)
+        return redirect('/app')
+    } catch (e) {
+        if (e instanceof MovieNoteError) {
+            return json<ActionData>({
+                error: e.message
+            }, { status: e.status })
+        }
+        return json<ActionData>({
+            error: (e as Error).message
+        }, { status: 400 })
+    }
+};
