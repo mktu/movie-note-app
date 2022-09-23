5d7974b172edf911bea2014f4d78d66c0677d6b2
mktu <mushi9ui@gmail.com>
2022-09-23 10:42:48 +0900

    save performance visibility on localstorage

--- app/providers/localstorage/useLocalstorage.ts
+++ app/providers/localstorage/useLocalstorage.ts
@@ -1,11 +1,21 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { LocalstorageType } from "./Context";
 
 const useLocalstorage = (localstorage: LocalstorageType) => {
 
-    const [sidebarWidth, setSidebarWidth] = useState(localstorage.getSidebarWidth())
-    const [visibleSidebarWidth, setVisibleSidebarWidth] = useState(localstorage.getVisibleSidebarWidth())
-    const [isKvDisabled, setKvDisabled] = useState(localstorage.isKvDisabled)
+    const [sidebarWidth, setSidebarWidth] = useState<number | null>(0)
+    const [visibleSidebarWidth, setVisibleSidebarWidth] = useState<number | null>(0)
+    const [isKvDisabled, setKvDisabled] = useState(false)
+    const [visiblePerformance, setVisiblePerformance] = useState(false)
+
+    // localstorage only works on the client side 
+    useEffect(() => {
+        setSidebarWidth(localstorage.getSidebarWidth())
+        setVisibleSidebarWidth(localstorage.getVisibleSidebarWidth())
+        setKvDisabled(localstorage.isKvDisabled)
+        setVisiblePerformance(localstorage.getVisiblePerformance())
+    }, [localstorage])
+
     const methods = useMemo(() => {
         return {
             getSidebarWidth: () => sidebarWidth,
@@ -22,9 +32,14 @@ const useLocalstorage = (localstorage: LocalstorageType) => {
             setKvDisabled: (b) => {
                 setKvDisabled(b)
                 localstorage.setKvDisabled(b)
-            }
+            },
+            saveVisiblePerformance: (b) => {
+                setVisiblePerformance(b)
+                localstorage.saveVisiblePerformance(b)
+            },
+            getVisiblePerformance: () => visiblePerformance
         } as typeof localstorage
-    }, [sidebarWidth, visibleSidebarWidth, isKvDisabled, localstorage])
+    }, [sidebarWidth, visibleSidebarWidth, isKvDisabled, localstorage, visiblePerformance])
 
     return methods
 }
--- utils/localstorage/index.ts
+++ utils/localstorage/index.ts
@@ -1,6 +1,7 @@
 const SIDEBAR_WIDTH_KEY = 'sidebar-width'
 const VISIBLE_SIDEBAR_WIDTH_KEY = 'visible-sidebar-width'
 const IS_NOTE_KV_DISABLED = 'note-kv-disabled'
+const VISIBLE_PERFORMANCE_KEY = 'visible-performance'
 
 function isNumeric(n: string) {
     return !isNaN(parseFloat(n)) && isFinite(Number(n));
@@ -30,4 +31,13 @@ export const isKvDisabled = () => {
 
 export const setKvDisabled = (disabled: boolean) => {
     localStorage.setItem(IS_NOTE_KV_DISABLED, String(disabled))
+}
+
+export const saveVisiblePerformance = (show: boolean) => {
+    localStorage.setItem(VISIBLE_PERFORMANCE_KEY, String(show))
+}
+
+export const getVisiblePerformance = () => {
+    const ret = localStorage.getItem(VISIBLE_PERFORMANCE_KEY)
+    return ret ? ret.toLowerCase() === 'true' : false
 }
