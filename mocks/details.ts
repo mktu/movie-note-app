b28890e73769cedcc3e7d355069cfc3dd50c3fb0
mktu <mushi9ui@gmail.com>
2022-11-06 21:18:41 +0900

    add seeds for e2e

--- mocks/details.ts
+++ mocks/details.ts
@@ -12,20 +12,72 @@ const detail: { [key: string]: TmdbDetail } = {
         backdrop_path: '/xKb6mtdfI5Qsggc44Hr9CCUDvaj.jpg',
         genres: [
             {
-                "id": 28,
-                "name": "アクション"
+                id: 28,
+                name: "アクション"
             },
             {
-                "id": 53,
-                "name": "スリラー"
+                id: 53,
+                name: "スリラー"
             },
             {
-                "id": 878,
-                "name": "サイエンスフィクション"
+                id: 878,
+                name: "サイエンスフィクション"
             }
         ],
         homepage: '',
         imdb_id: 'tt0103064',
+    },
+    616037: {
+        id: '616037',
+        title: "ソー：ラブ＆サンダー",
+        release_date: "2022-07-06",
+        status: "Released",
+        lng: 'ja',
+        overview: "壮絶なバトルの末、宇宙に旅立ったソー(クリス・ヘムズワース)は、すっかり戦いから遠ざかっていた。ある日、神殺しをもくろむ強敵ゴア(クリスチャン・ベイル)が出現し、ソーと新たに王となったヴァルキリー(テッサ・トンプソン)が応戦するものの、ゴアの前に全く歯が立たなかった。そこへマイティ・ソーのコスチュームをまとったソーの元恋人ジェーン(ナタリー・ポートマン)が現れ、ソーとヴァルキリーに協力する。",
+        poster_path: "/e7elkgJQDCIS4m30AJTwGFE5JGf.jpg",
+        backdrop_path: "/jsoz1HlxczSuTx0mDl2h0lxy36l.jpg",
+        genres: [
+            {
+                id: 14,
+                name: "ファンタジー"
+            },
+            {
+                id: 28,
+                name: "アクション"
+            },
+            {
+                id: 35,
+                name: "コメディ"
+            }
+        ],
+        homepage: "https://marvel.disney.co.jp/movie/thor-love-and-thunder",
+        imdb_id: "tt10648342",
+    },
+    526896: {
+        id: '526896',
+        title: "モービウス",
+        status: "Released",
+        release_date: "2022-03-30",
+        lng: 'ja',
+        overview: "珍しい血液の病気にかかったマイケル・モービュース博士は、自分と同じ運命にある人々を救うために、決死の覚悟で賭けに出る。しかし、最初は大成功に見えたが、やがて病気よりも悪い治療法であることが判明する。",
+        homepage: "https://www.sonypictures.jp/he/2315289/",
+        backdrop_path: "/jzWT0zd8U77fqWg5WgUfYaMzSFz.jpg",
+        poster_path: "/nm3cQlgz0BfQTtG6Cqg63pAREaY.jpg",
+        genres: [
+            {
+                id: 28,
+                name: "アクション"
+            },
+            {
+                id: 878,
+                name: "サイエンスフィクション"
+            },
+            {
+                id: 14,
+                name: "ファンタジー"
+            }
+        ],
+        imdb_id: "tt5108870",
     }
 }
 
