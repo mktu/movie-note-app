265b85d538bf00bb720900ae2f4dca9b79877f15
mktu <mushi9ui@gmail.com>
2022-11-12 23:55:22 +0900

    scrape in edge

--- functions/api/imdb/[id].ts
+++ functions/api/imdb/[id].ts
@@ -1,22 +1,30 @@
 import { json } from "@remix-run/cloudflare"
 import { getImdbRateKv, putImdbRateKv } from "~/features/movie-note/features/imdb/server/kv"
-import type { ImdbRate } from "~/features/movie-note/features/imdb/types"
+import { parseRating } from "~/features/movie-note/features/imdb/server/parser"
+import { ScrapeError } from "~/features/movie-note/features/imdb/server/exception"
+import { parse } from 'node-html-parser'
 
 
 export const onRequestGet: PagesFunction<{
-    IMDB_SCRAPER_URL: string,
     ImdbInfo: KVNamespace
 }> = async ({ params, env }) => {
 
-    const url = env.IMDB_SCRAPER_URL
     const { id } = params as { id: string }
     const cache = await getImdbRateKv(env.ImdbInfo, id)
     if (cache) {
         return json({ ...cache, cache: true })
     }
-    const res = await fetch(`${url}/${id}`)
-    const data = await res.json<ImdbRate>()
-
+    const res = await fetch(`https://www.imdb.com/title/${id}`)
+    if (res?.status !== 200) {
+        throw new ScrapeError(`${id} is not found`, res?.status || 404)
+    }
+    const body = await res.text()
+    const root = parse(body)
+    const text = root.querySelector('[aria-label="View User Ratings"]')?.firstChild.innerText
+    if (!text) {
+        throw new ScrapeError(`cannnot scrape ${id}`, 500)
+    }
+    const data = parseRating(text)
     if (!cache) {
         await putImdbRateKv(env.ImdbInfo, id, data)
     }
