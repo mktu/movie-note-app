c6c7e6b35933c7bca66f3e75cf249933bb9abc99
mktu <mushi9ui@gmail.com>
2023-03-15 21:35:49 +0900

    fix dirty algorithm

--- app/features/movie-note/hooks/useMovieNoteChangeMonitor.ts
+++ app/features/movie-note/hooks/useMovieNoteChangeMonitor.ts
@@ -1,14 +1,19 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { usePrompt } from "~/hooks/useBlocker"
 
 const useMovieNoteChangeMonitor = () => {
     const { t } = useTranslation('common')
     const [dirty, setDirty] = useState(false)
+    const checkDirty = useCallback((text: string) => {
+        const regex = /"text":"([^"]*)"/;
+        const match = text.match(regex);
+        return Boolean(match && match.length > 1)
+    }, [])
     const { unblock } = usePrompt(t('leave-screen'), dirty)
 
     return {
-        setDirty, unblock
+        setDirty, unblock, checkDirty
     }
 }
 
