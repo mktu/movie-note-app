288ed884f9b4d837ae56f5a4a640cd7c389894c3
mktu <mushi9ui@gmail.com>
2023-10-25 23:45:26 +0900

    upgrade remix v2

--- app/features/rte/components/Container.tsx
+++ app/features/rte/components/Container.tsx
@@ -1,21 +1,15 @@
 import type { ComponentProps, FC } from 'react';
-import Editor from './Editor';
-// import { lazy, Suspense } from 'react'
-// const Editor = lazy(() => import('./Editor'));
+import { lazy, Suspense } from 'react'
+const Editor = lazy(() => import('./Editor'));
 
 type Props = ComponentProps<typeof Editor>
 
 const Container: FC<Props> = (props) => {
     return (
-        <Editor {...props} />
-    )
-    // cannot use renderToReadableStream 
-    // (related isue: https://github.com/remix-run/remix/issues/2299#issuecomment-1091149019)
-    // return (
-    //     <Suspense fallback={<div />}>
-    //         <Editor {...props} />
-    //     </Suspense>
-    // );
+        <Suspense fallback={<div />}>
+            <Editor {...props} />
+        </Suspense>
+    );
 };
 
 export default Container;
