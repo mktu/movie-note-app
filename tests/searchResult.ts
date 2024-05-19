9f62b9c8ba0266bd79570cad448fb634569258b5
mktu <mushi9ui@gmail.com>
2024-05-19 23:08:04 +0900

    mock search result

---
+++ tests/searchResult.ts
@@ -0,0 +1,399 @@
+export const getSearchResultMockData = () => ({
+    "page": 1,
+    "results": [
+        {
+            "adult": false,
+            "backdrop_path": "/6dpnthrofbqWmvapUyvqCrkdHG.jpg",
+            "genre_ids": [
+                28,
+                53,
+                878
+            ],
+            "id": 218,
+            "original_language": "en",
+            "original_title": "The Terminator",
+            "overview": "アメリカのとある街、深夜突如奇怪な放電と共に屈強な肉体をもった男が現れる。同じくして放電の中からもう一人の男カイル・リースが現れる。屈強な肉体を持った男はモラルや常識もない。あるのはただ１つの目的アメリカ人女性サラ・コナーという名の人物の殺害だった。電話帳名簿から「サラ・コナー」の名を持つ女性をかたっぱしから銃殺していく男。その頃カイルは目的のサラ・コナーと接触し間一髪で彼女を救う。カイルはサラに、サラを狙っているのは近未来から送られた人類殺戮ロボット「ターミネーター」であり、未来ではロボットの反乱による機械対人類の最終戦争が起こっている事、そしてサラは人類軍の希望のリーダー　ジョン・コナーの母親である事を告げる。　サラを連れカイルはターミネーターからの逃亡を開始する。",
+            "popularity": 421.882,
+            "poster_path": "/aGZGn8ETH9BeQhHJFTFjyZyoCLq.jpg",
+            "release_date": "1984-10-26",
+            "title": "ターミネーター",
+            "video": false,
+            "vote_average": 7.665,
+            "vote_count": 12542
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/g4a5YLWwi6OCp8TcvxsUNrXMbN5.jpg",
+            "genre_ids": [
+                878,
+                28,
+                53,
+                12
+            ],
+            "id": 87101,
+            "original_language": "en",
+            "original_title": "Terminator Genisys",
+            "overview": "2029年。機械軍と人類の死闘は人類の勝利に終わりつつあった。だが、機械軍は人類の指導者ジョン・コナーの母サラを暗殺して歴史を書き換えるべく、殺人ロボット・ターミネーターT－800をタイムマシンで過去へ送る。一方人類側も、サラを救う任務に志願した青年カイルを過去へと飛ばす。カイルは計画通り1984年に到着するが、そこにはすでに未来を知って戦士となったサラと、彼女の守護者である別のT－800がいた。",
+            "popularity": 203.28,
+            "poster_path": "/5gNGTawjkPW9PIsjkG4YTLzoUOP.jpg",
+            "release_date": "2015-06-23",
+            "title": "ターミネーター：新起動／ジェニシス",
+            "video": false,
+            "vote_average": 5.94,
+            "vote_count": 8151
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/a6cDxdwaQIFjSkXf7uskg78ZyTq.jpg",
+            "genre_ids": [
+                878,
+                28
+            ],
+            "id": 290859,
+            "original_language": "en",
+            "original_title": "Terminator: Dark Fate",
+            "overview": "人類滅亡の日である「審判の日」は回避されたが、まだ危機は去っていなかった。メキシコシティで父と弟とごく普通の生活を送っていた21歳の女性ダニーのもとに、未来から最新型ターミネーター「REV-9」が現れ、彼女の命を狙う。一方、同じく未来からやってきたという女性戦士グレースが、ダニーを守るためにREV-9と壮絶な戦いを繰り広げる。何度倒しても立ち上がってくるREV-9にダニーとグレースは追いつめられるが、そこへ、かつて人類を滅亡の未来から救ったサラ・コナーが現れる。",
+            "popularity": 162.048,
+            "poster_path": "/s19SKtVpBs4zalNUyvsvR8OEilg.jpg",
+            "release_date": "2019-10-23",
+            "title": "ターミネーター：ニュー・フェイト",
+            "video": false,
+            "vote_average": 6.454,
+            "vote_count": 4884
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/8MksSPtPvqcSDruLdpibPmTP7LY.jpg",
+            "genre_ids": [
+                28,
+                53,
+                878
+            ],
+            "id": 280,
+            "original_language": "en",
+            "original_title": "Terminator 2: Judgment Day",
+            "overview": "未来からの抹殺兵器ターミネーターを破壊し、近未来で恐ろしい戦争が起こる事を知ってしまったサラ・コナー。カイルとの子供ジョンは母親から常にその戦争の話や戦いへの備えの話を聞かされていた。サラは周囲から変人扱いされ精神病院へ収容されジョンは親戚の家で暮らしていた。ある日ジョンの前に執拗にジョンを狙う不審な警官が現る。軌道を逸した警官の行動は明らかにジョンを殺害しようとしていた。殺されるその寸前、見知らぬ屈強な男が現れジョンを救う。彼は自らをターミネーターでありジョンを守るべく再プログラムされ未来から送り込まれたと告げる。ジョンは病院の母親を連れ出し、最終戦争を起こす原因であるコンピューターシステム「スカイネット」を破壊するため、ターミネーターと共にサイバーダイン社へ向かうが・・・",
+            "popularity": 142.335,
+            "poster_path": "/p4MBs36yK5o4OHuL3a1ZOr59hda.jpg",
+            "release_date": "1991-07-03",
+            "title": "ターミネーター2",
+            "video": false,
+            "vote_average": 8.115,
+            "vote_count": 12372
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/5tKiuZvvV1lic7v65rdoGPmoOvf.jpg",
+            "genre_ids": [
+                28,
+                878,
+                53
+            ],
+            "id": 534,
+            "original_language": "en",
+            "original_title": "Terminator Salvation",
+            "overview": "“審判の日”から10年後の2018年。人類軍の指導者となり、機械軍と戦うことを幼いころから運命づけられてきたジョン・コナー。今や30代となった彼は、人類滅亡をもくろむスカイネットの猛攻が開始されようとする中、ついに人類軍のリーダーとして立ち上がることになる。",
+            "popularity": 64.796,
+            "poster_path": "/7fmBDyHsLTyhfRSZk19wrY23zDg.jpg",
+            "release_date": "2009-05-20",
+            "title": "ターミネーター4",
+            "video": false,
+            "vote_average": 6.058,
+            "vote_count": 6258
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/kbXMOnz2RhTSAbLtHX5hy5AXtwv.jpg",
+            "genre_ids": [
+                28,
+                53,
+                878
+            ],
+            "id": 296,
+            "original_language": "en",
+            "original_title": "Terminator 3: Rise of the Machines",
+            "overview": "スカイネットを破壊、予見されていた最終戦争の日も過ぎたが、ジョンの心には母親から刷り込まれた戦争の話が無くなったとは思えず不安な毎日を過ごしていた。そんな中、未来より新たなターミネーターがジョンの仲間となる兵士達の抹殺のために送られてくる。新型ターミネーターT-Xは、ジョンを発見したことで、ジョン自身の殺害プログラムを優先し彼を追うが、たまたまいっしょにいた女性と共に別のターミネーターT-850に救われる。T-850は未来のジョンの妻ケイトによりリプログラムされ送られたのだった。ジョンとケイトは最終戦争開始の日である審判の日は回避することはできず、スカイネット自体も破壊を免れており起動を待ち軍事基地に現存している事を知らされる。スカイネット起動時間が迫るなか、ジョンとケイトは起動を阻止すべくT-850の指示のもと、アメリカ軍基地へ向かうが・・",
+            "popularity": 51.29,
+            "poster_path": "/nRSkgx2MS4ZZkKG3K8ZVPn3ZgRB.jpg",
+            "release_date": "2003-07-02",
+            "title": "ターミネーター3",
+            "video": false,
+            "vote_average": 6.159,
+            "vote_count": 6389
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/puiwodUrPSDTkvlIRr2CdDM1isB.jpg",
+            "genre_ids": [
+                28,
+                27,
+                878
+            ],
+            "id": 26342,
+            "original_language": "en",
+            "original_title": "The Terminators",
+            "overview": "A small band of resistance fighters battle the cyborgs that have taken control of the planet.",
+            "popularity": 10.755,
+            "poster_path": "/7LV3sg3bJOGFD6J0gn7Wt1IL5SK.jpg",
+            "release_date": "2009-04-28",
+            "title": "The Terminators",
+            "video": false,
+            "vote_average": 3.9,
+            "vote_count": 56
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/2cGJyY6RpEDbSiyn4jrCFWqghyf.jpg",
+            "genre_ids": [
+                28
+            ],
+            "id": 40027,
+            "original_language": "en",
+            "original_title": "Ninja Terminator",
+            "overview": "Three martial-arts students search for the Golden Ninja Warrior, a statue reputed to have magic powers.",
+            "popularity": 7.159,
+            "poster_path": "/5YkYpKxTQVq4gna3qTztwwwo8YF.jpg",
+            "release_date": "1985-01-01",
+            "title": "Ninja Terminator",
+            "video": false,
+            "vote_average": 5.8,
+            "vote_count": 37
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/kWH43qHgNatlZ1DEpuxqNqiRbbO.jpg",
+            "genre_ids": [
+                99
+            ],
+            "id": 473793,
+            "original_language": "en",
+            "original_title": "The Making of 'Terminator 2: Judgment Day'",
+            "overview": "Documentary of the making of the sequel to the popular Schwarzenegger film, The Terminator.",
+            "popularity": 6.586,
+            "poster_path": "/m0k3vRqh4eVZONufDahNufHi8Em.jpg",
+            "release_date": "1991-01-01",
+            "title": "The Making of 'Terminator 2: Judgment Day'",
+            "video": false,
+            "vote_average": 7.1,
+            "vote_count": 18
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/mdmO1o8HhmOKCadFhDXj3TUNlZ.jpg",
+            "genre_ids": [
+                28,
+                878,
+                27
+            ],
+            "id": 66664,
+            "original_language": "it",
+            "original_title": "Terminator 2",
+            "overview": "In a polluted future Venice researchers work to improve the situation. One day, unknown forces start killing them. A team of soldiers and a couple of civilians is sent to investigate. Soon, they encounter strange murderous creatures.",
+            "popularity": 8.586,
+            "poster_path": "/tztmdSyvfvQBVyzqyh6sKQeJP5g.jpg",
+            "release_date": "1989-03-17",
+            "title": "Shocking Dark",
+            "video": false,
+            "vote_average": 4.7,
+            "vote_count": 47
+        },
+        {
+            "adult": false,
+            "backdrop_path": null,
+            "genre_ids": [
+                28,
+                12,
+                14
+            ],
+            "id": 576605,
+            "original_language": "en",
+            "original_title": "Mortal Kombat: Terminator",
+            "overview": "",
+            "popularity": 6.572,
+            "poster_path": "/6rUGUUVvA9U8V9JI5Vay8mkyMdJ.jpg",
+            "release_date": "1998-10-03",
+            "title": "Mortal Kombat: Terminator",
+            "video": true,
+            "vote_average": 6,
+            "vote_count": 11
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/eNO76RvyKyzrEY4nhhtzM8feBng.jpg",
+            "genre_ids": [
+                35,
+                878,
+                28
+            ],
+            "id": 717823,
+            "original_language": "ru",
+            "original_title": "Terminator 5: Смерть Голливуда",
+            "overview": "An unofficial sequel to Kefir-Profit's unofficial Terminator sequel... This time featuring Superman, Rambo, Neo from the Matrix, Harry Potter and the evil Agent 007.",
+            "popularity": 1.295,
+            "poster_path": "/7K84p0Tgo9wnR7ReBwnOoVfmXTk.jpg",
+            "release_date": "2004-01-01",
+            "title": "Terminator 5: The Death of Hollywood",
+            "video": false,
+            "vote_average": 5.9,
+            "vote_count": 10
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/hBU2INkYLe3OBKZspT0TRd3nTxX.jpg",
+            "genre_ids": [
+                28,
+                27,
+                14,
+                878
+            ],
+            "id": 61904,
+            "original_language": "en",
+            "original_title": "Pembalasan Ratu Pantai Selatan",
+            "overview": "The spirit of an ancient evil queen posesses the body of a young anthropological student, who then goes on a murderous rampage.",
+            "popularity": 8.908,
+            "poster_path": "/lwOK1GHK8KRgiWhfBkEnQKPLgEi.jpg",
+            "release_date": "1989-06-10",
+            "title": "Lady Terminator",
+            "video": false,
+            "vote_average": 5.8,
+            "vote_count": 53
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/tLAYqqrkaQzi6JgaSLJvfTwil1J.jpg",
+            "genre_ids": [
+                99
+            ],
+            "id": 503718,
+            "original_language": "en",
+            "original_title": "T2: Reprogramming The Terminator",
+            "overview": "This retrospective documentary focuses on the cast and crew reminiscing about the making of the film and its legacy. It features new interviews James Cameron, William Wisher, Arnold Schwarzenegger, Robert Patrick, Joe Morton, Edward Furlong, Brad Fiedel, Mario Kassar, Stephanie Austin, Adam Greenberg and Dennis Muren.",
+            "popularity": 5.348,
+            "poster_path": "/vQUbuyFslWuRfCd44uaba9Qf0ev.jpg",
+            "release_date": "2017-11-23",
+            "title": "T2: Reprogramming The Terminator",
+            "video": false,
+            "vote_average": 7.607,
+            "vote_count": 14
+        },
+        {
+            "adult": false,
+            "backdrop_path": null,
+            "genre_ids": [
+                28,
+                878,
+                53
+            ],
+            "id": 233341,
+            "original_language": "en",
+            "original_title": "Terminator the Second",
+            "overview": "Terminator the Second is the premiere production of the Nashville performance collective, Husky Jackal Theater. A product of nearly a year of research and revision, the script tells the story of a boy and his cyborg protector entirely in lines and phrases taken from the plays of William Shakespeare. Each line and phrase is taken from original folios, with only proper nouns, pronouns and corresponding verb tenses subject to change. In doing so, the authors were able to accurately recreate the story of Terminator 2: Judgement Day, while remaining true to the words of Shakespeare in form, if less so in intent.",
+            "popularity": 1.462,
+            "poster_path": null,
+            "release_date": "2013-11-01",
+            "title": "Terminator the Second",
+            "video": false,
+            "vote_average": 5.3,
+            "vote_count": 10
+        },
+        {
+            "adult": false,
+            "backdrop_path": null,
+            "genre_ids": [
+                28,
+                35,
+                27
+            ],
+            "id": 965618,
+            "original_language": "fi",
+            "original_title": "Video Cop: Terminator Commando",
+            "overview": "Jake Crusher is a cop from the 80's who gets sucked into a VCR where he becomes Video Cop.",
+            "popularity": 0.408,
+            "poster_path": "/ge2YeTh5DpdxzS32u1eHPIGV36w.jpg",
+            "release_date": "2017-08-25",
+            "title": "Terminator Commando",
+            "video": false,
+            "vote_average": 6,
+            "vote_count": 2
+        },
+        {
+            "adult": false,
+            "backdrop_path": null,
+            "genre_ids": [
+                99
+            ],
+            "id": 832113,
+            "original_language": "en",
+            "original_title": "No Feat But What We Make: 'Terminator 2' and the Rise of Digital Effects",
+            "overview": "A look at the digital effects in Terminator 2: Judgment Day.",
+            "popularity": 3.752,
+            "poster_path": "/wx52hf0TQzlyjjyUZYrRJbxmZFh.jpg",
+            "release_date": "2003-06-03",
+            "title": "No Feat But What We Make: 'Terminator 2' and the Rise of Digital Effects",
+            "video": false,
+            "vote_average": 6,
+            "vote_count": 2
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/2JNljYdphbMWX1Yq2JuusKvJyJc.jpg",
+            "genre_ids": [
+                16,
+                28,
+                12
+            ],
+            "id": 222148,
+            "original_language": "en",
+            "original_title": "Terminator Salvation: The Machinima Series",
+            "overview": "In the Year 2016, Resistance fighter Blair Williams embarks on a deadly mission to search for a threat that is weakening humanity's defense against the self-aware artificial intelligence called Skynet and it's lethal Terminators.",
+            "popularity": 3.971,
+            "poster_path": "/6yBe7QnXbsEFLEsjxa3fYcxxbZp.jpg",
+            "release_date": "2009-05-18",
+            "title": "Terminator Salvation: The Machinima Series",
+            "video": false,
+            "vote_average": 6.2,
+            "vote_count": 27
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/erWRBbDpb1tQBqgHyQzRnoVfTyM.jpg",
+            "genre_ids": [
+                878,
+                16,
+                27
+            ],
+            "id": 1078179,
+            "original_language": "en",
+            "original_title": "Terminator: Hunter Killer",
+            "overview": "TERMINATOR: HUNTER KILLER is a proof-of-concept short film, set in the future war of the Terminator-movie universe, as the humans struggle to survive in the post-apocalyptic wasteland following 'Judgment Day'.",
+            "popularity": 1.603,
+            "poster_path": "/pD3YUsXZ4SGuwQkmyfqZr9ln1GK.jpg",
+            "release_date": "2021-09-07",
+            "title": "Terminator: Hunter Killer",
+            "video": false,
+            "vote_average": 9.3,
+            "vote_count": 3
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/9L21PeX8Sl79svpf0zgMhgLC3rV.jpg",
+            "genre_ids": [
+                878,
+                27
+            ],
+            "id": 133360,
+            "original_language": "en",
+            "original_title": "Alien Terminator",
+            "overview": "Five miles below the Earth's surface, a group of scientists is working in self-imposed isolation, perfecting a series of DNA experiments that could literally change the world. But when one of those experiments goes awry, it unleashes a nearly immortal organism--one capable of instant regeneration, and with an insatiable appetite for living flesh. Soon, nothing can contain the creature, and with the lab's life support systems quickly shutting down, the scientists grimly prepare for their final battle...  A small group of scientists are working in self-imposed isolation below the Earth's surface. They are all working to create DNA which could change the world, but one of the scientists have been given an secret assignment, and when the assignment goes wrong, all hell breaks lose. The new creation is now aboard the ship and will destroy anyone who gets in its way, as it moves towards the surface.",
+            "popularity": 3.377,
+            "poster_path": "/onYNCujqToCWgPsYgu451oTlwrH.jpg",
+            "release_date": "1996-10-09",
+            "title": "Alien Terminator",
+            "video": false,
+            "vote_average": 4.3,
+            "vote_count": 23
+        }
+    ],
+    "total_pages": 6,
+    "total_results": 103
+})
